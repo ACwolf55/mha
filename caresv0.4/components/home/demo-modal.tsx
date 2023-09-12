e5f0f82e0b257b1c8730077d2abb55d6aca07303
tyler-xyz <77706863+tyler-xyz@users.noreply.github.com>
2023-09-12 21:58:58 +0000

    style changes. added new style props to layout.css

--- caresv0.4/components/home/demo-modal.tsx
+++ caresv0.4/components/home/demo-modal.tsx
@@ -19,19 +19,28 @@ const DemoModal = ({
     <Modal showModal={showDemoModal} setShowModal={setShowDemoModal}>
       <div className="w-full overflow-hidden md:max-w-md md:rounded-2xl md:border md:border-gray-100 md:shadow-xl">
         <div className="flex flex-col items-center justify-center space-y-3 bg-white px-4 py-6 pt-8 text-center md:px-16">
-          <a href="https://precedent.dev">
+          <a href="https://justtellone.org">
             <Image
               src="/logo.png"
-              alt="Precedent Logo"
-              className="h-10 w-10 rounded-full"
-              width={20}
-              height={20}
+              alt="JustTellOne.org"
+              width={148}
+              height={80}
             />
           </a>
-          <h3 className="font-display text-2xl font-bold">Precedent</h3>
-          <p className="text-sm text-gray-500">
-            Precedent is an opinionated collection of components, hooks, and
-            utilities for your Next.js project.
+          <h3 className="font-display text-2xl font-bold">End-User License Agreement</h3>
+          <p className="text-xsm text-gray-500 text-left">
+          This End-User License Agreement (this “EULA”) is a legal agreement between you (“Licensee”) and
+Mental Health Advocates of WNY (“Licensor”), the author of JustTellOne, including all HTML files, XML
+files, Java files, graphics files, animation files, data files, technology, development tools, scripts and
+programs, both in object code and source code (the “Software”), the deliverables provided pursuant to
+this EULA, which may include associated media, printed materials, and “online” or electronic
+documentation.
+<br></br>
+By installing, copying, or otherwise using the Software, Licensee agrees to be bound by the terms and
+conditions set forth in this EULA. If Licensee does not agree to the terms and conditions set forth in this
+EULA, then Licensee may not download, install, or use the Software.
+<br></br>
+
           </p>
         </div>
       </div>
--- caresv0.4/components/layout/footer.tsx
+++ caresv0.4/components/layout/footer.tsx
@@ -1,12 +1,17 @@
+const styles = {
+  footer: {
+    backgroundColor: "#462d91",
+  },
+};
 export default function Footer() {
   return (
-    <div className="absolute w-full border-t border-gray-200 bg-#462d91 py-5 text-center">
+    <div className="absolute w-full border-t border-gray-200 bg-opacity-100 bg-purple py-5 text-center">
       <p className="text-white">
         More Questions?{" "}
         <a
           className="font-medium text-white underline transition-colors"
           href="https://justtellone.org/about/"
-          target="_self"
+          target="_blank"
           rel="noopener noreferrer"
         >
           Read more on our website
