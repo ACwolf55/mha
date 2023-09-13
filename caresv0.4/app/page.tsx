201c54e8630fa98d791fbc89badda8ccfc8592b2
tyler-xyz <77706863+tyler-xyz@users.noreply.github.com>
2023-09-13 19:38:59 +0000

    global.css, rmvd elements from compgrid ^ page.tsx

--- caresv0.4/app/page.tsx
+++ caresv0.4/app/page.tsx
@@ -100,7 +100,7 @@ const features = [
   {
     title: "Beautiful, reusable components",
     description:
-      "Pre-built beautiful, a11y-first components, powered by [Tailwind CSS](https://tailwindcss.com/), [Radix UI](https://www.radix-ui.com/), and [Framer Motion](https://framer.com/motion)",
+      "Pre-built beautiful, a11y-first components, powered by [Tailwind CSS](https://tailwindcss.com/), [Radix UI](https://www.radix-ui.com/)",
     large: true,
   }
 ];
