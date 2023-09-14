ec65e74999e305e33801e055d9e9a099286d3d4d
tyler-xyz <77706863+tyler-xyz@users.noreply.github.com>
2023-09-14 22:07:20 +0000

    changes

--- caresv0.4/app/chat/page.tsx
+++ caresv0.4/app/chat/page.tsx
@@ -1,9 +1,6 @@
 
 import Card from "@/components/home/card";
 import Balancer from "react-wrap-balancer";
-import { DEPLOY_URL } from "@/lib/constants";
-import { Github, Twitter } from "@/components/shared/icons";
-import WebVitals from "@/components/home/web-vitals";
 import ComponentGrid from "@/components/home/component-grid";
 import Image from "next/image";
 import { nFormatter } from "@/lib/utils";
@@ -19,8 +16,7 @@ export default function Chat() {
       </div>
       {/* begin card component */}
       <div className="my-10 grid w-full max-w-screen-xl animate-fade-up grid-cols-1 gap-5 px-5 md:grid-cols-1 xl:px-0">
-          <Card />
-
+          <Card title={""} description={""} demo={undefined} />
       </div>
     </>
   );
