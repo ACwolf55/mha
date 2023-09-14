147d1d533b47f9690eff0ee29348636028b8f0fb
tyler-xyz <77706863+tyler-xyz@users.noreply.github.com>
2023-09-14 00:24:30 +0000

    py-22 created to change bottom padding

--- caresv0.4/app/layout.tsx
+++ caresv0.4/app/layout.tsx
@@ -28,7 +28,7 @@ export default async function RootLayout({
           {/* @ts-expect-error Server Component */}
           <Nav />
         </Suspense>
-        <main className="flex min-h-screen w-full flex-col items-center justify-center py-20">
+        <main className="flex min-h-screen w-full flex-col items-center justify-center py-22">
           {children}
         </main>
         <Footer />
