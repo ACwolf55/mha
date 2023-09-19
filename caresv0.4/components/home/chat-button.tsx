0af1787c0c7fdc17ac8df40abe4c240458fc0a24
tyler-xyz <77706863+tyler-xyz@users.noreply.github.com>
2023-09-18 23:51:38 -0400

    Create chat-button.tsx
    created replacement lets chat button using specific @twilio items

---
+++ caresv0.4/components/home/chat-button.tsx
@@ -0,0 +1,9 @@
+import {Button} from '@twilio-paste/button';
+import { ProductChatIcon } from "@twilio-paste/icons/esm/ProductChatIcon";
+
+const LetsChat = () => (
+  <Button variant="destructive" size="rounded_small">
+    Let's Chat
+    <ProductChatIcon decorative />
+  </Button>
+);
