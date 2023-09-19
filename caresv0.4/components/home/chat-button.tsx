e618a055f5539e584bd4db95fdf45048d40349cc
tyler-xyz <77706863+tyler-xyz@users.noreply.github.com>
2023-09-19 00:04:45 -0400

    Update chat-button.tsx
    see previous

--- caresv0.4/components/home/chat-button.tsx
+++ caresv0.4/components/home/chat-button.tsx
@@ -1,9 +1,12 @@
 import {Button} from '@twilio-paste/button';
 import { ProductChatIcon } from "@twilio-paste/icons/esm/ProductChatIcon";
 
-const LetsChat = () => (
+const LetsChat = () => {
+  return(
   <Button variant="destructive" size="rounded_small">
     Let's Chat
     <ProductChatIcon decorative />
   </Button>
 );
+}
+export default LetsChat;
