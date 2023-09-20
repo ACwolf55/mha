762ccd36fdd13110d91fc29e6d033097d856a364
tyler-xyz <77706863+tyler-xyz@users.noreply.github.com>
2023-09-20 04:40:03 +0000

    inserted chat-dialog

--- caresv0.4/app/chat/page.tsx
+++ caresv0.4/app/chat/page.tsx
@@ -1,14 +1,10 @@
-
-import Card from "@/components/home/card";
-import Balancer from "react-wrap-balancer";
-import ComponentGrid from "@/components/home/component-grid";
 import Image from "next/image";
 import { nFormatter } from "@/lib/utils";
 import React from "react";
-import { Chat } from "@twilio-paste/core";
-import { ChatMessage } from "@twilio-paste/core";
 import { Conversation } from "@twilio/conversations";
 import { ChatComposer } from "@twilio-paste/core";
+import { Box } from "framer-motion";
+
 
 /* this is the index page for the Chat Page */
 
@@ -17,7 +13,7 @@ export default function Chat() {
     <>
       {/* begin card component */}
       <div className="my-10 grid w-full max-w-screen-xl animate-fade-up grid-cols-1 gap-5 px-5 md:grid-cols-1 xl:px-0">
-          <Card title={""} description={""} demo={undefined} />
+
       </div>
     </>
   );
